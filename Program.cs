39748921ff91fc2390cd74fa60809ab83368e242
7azemIbrahim <121644999+7azemIbrahim@users.noreply.github.com>
2022-12-30 01:37:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N1 : ");
+            int N1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter N2  : ");
+            int N2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = N1; i <= N2; i++)
+            {
+                for (int g = 1; g < i; g++)
+                {
+                    if (i % g == 0)
+                    {
+                        sum = g + sum;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                    sum = 0;
+            }
+        }
+    }
+}
